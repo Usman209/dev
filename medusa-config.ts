340463f95ddccf8be29799ab1dd273a5cb2d459a
Usman Jamil <UJamil@lmkr.com>
2025-04-17 14:07:07 +0500

    code

--- medusa-config.ts
+++ medusa-config.ts
@@ -7,9 +7,9 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS! || "*",
-      adminCors: process.env.ADMIN_CORS! || "*",
-      authCors: process.env.AUTH_CORS! || "*",
+      storeCors:  "*",
+      adminCors:  "*",
+      authCors:  "*",
       jwtSecret: process.env.JWT_SECRET! || 'fadsfadf3244sd',
       cookieSecret: process.env.COOKIE_SECRET! || 'supersecret',
     },
